6cede0bf0843deb092f3e1717f643a3e04ae21b3
ffukuda23 <ffukuda@gmail.com>
2026-09-06 20:07:31 -0300

    Trocar fonte de titulos (Syne -> Inter) para harmonizar com o logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -87,7 +87,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Syne:wght@400;600;700;800&family=DM+Sans:ital,wght@0,300;0,400;0,500;1,300&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&family=DM+Sans:ital,wght@0,300;0,400;0,500;1,300&display=swap"
           rel="stylesheet"
         />
         <script
